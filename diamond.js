b1f67cdab311fb7879802af48fff7e984be55056
practiceandcode <contact@practiceandcode.com>
2026-01-19 18:26:59 -0500

    Add Print Diamond

---
+++ diamond.js
@@ -0,0 +1,20 @@
+/*
+Write a JavaScript program that prints the following output:
+   /\
+  /  \
+ /    \
+/      \
+\      /
+ \    /
+  \  /
+   \/
+*/
+
+console.log("   /\\")
+console.log("  /  \\")
+console.log(" /    \\")
+console.log("/      \\")
+console.log("\\      /")
+console.log(" \\    /")
+console.log("  \\  / ")
+console.log("   \\/  ")
