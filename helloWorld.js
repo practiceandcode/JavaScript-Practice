90b7826ac2c09e854e461d9ffa8730b3632cc5a0
practiceandcode <contact@practiceandcode.com>
2026-01-19 17:47:32 -0500

    Add Print Hello World

---
+++ helloWorld.js
@@ -0,0 +1,11 @@
+/*
+Write a JavaScript program that prints the following output:
+
+==================
+|| HELLO WORLD! ||
+==================
+*/
+
+console.log("==================")
+console.log("|| HELLO WORLD! ||")
+console.log("==================")
